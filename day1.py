2eb73189b529ce751cafcef3a32d139652b8f3a6
Alex Villarreal <alex@alexvy86.com>
2020-12-02 18:40:35 +0000

    Day 1 - part 2

--- day1.py
+++ day1.py
@@ -17,3 +17,22 @@ for currentIndex1 in range(0, numOfExpenses - 1):
         break
 
 print(expense1 * expense2)
+
+expense1 = 0
+expense2 = 0
+expense3 = 0
+
+for currentIndex1 in range(0, numOfExpenses - 2):
+    expense1 = expenses[currentIndex1]
+    for currentIndex2 in range(currentIndex1 + 1, numOfExpenses - 1):
+        expense2 = expenses[currentIndex2]
+        for currentIndex3 in range(currentIndex2 + 1, numOfExpenses):
+            expense3 = expenses[currentIndex3]
+            if expense1 + expense2 + expense3 == 2020:
+                break
+        if expense1 + expense2 + expense3 == 2020:
+            break
+    if expense1 + expense2 + expense3 == 2020:
+        break
+
+print(expense1 * expense2 * expense3)

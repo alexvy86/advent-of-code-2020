9b8b425d901ba9499330bb3afb3f362b4ccd757c
Alex Villarreal <alex@alexvy86.com>
2020-12-02 21:21:31 +0000

    Day 2 - part 1

---
+++ day2.py
@@ -0,0 +1,21 @@
+from utils import read_lines
+import re
+
+numOfValidPasswords = 0
+
+regex = re.compile(r"(?P<minTimes>\d+)\-(?P<maxTimes>\d+) (?P<character>\w)\: (?P<password>\w+)")
+for x in read_lines(2):
+    parsed = regex.match(x)
+    minTimes = int(parsed.group('minTimes'))
+    maxTimes = int(parsed.group('maxTimes'))
+    character = parsed.group('character')
+    password = parsed.group('password')
+
+    numOfCharInPassword = len(re.findall(f"{character}", password))
+
+    # print(f"{minTimes} {maxTimes} {character} {numOfCharInPassword} {password}")
+
+    if minTimes <= numOfCharInPassword <= maxTimes:
+        numOfValidPasswords += 1
+
+print(numOfValidPasswords)

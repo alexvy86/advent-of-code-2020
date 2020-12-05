af97becdc76d067e372395458f2f87a0e30a9309
Alex Villarreal <alex@alexvy86.com>
2020-12-05 22:24:16 +0000

    Day 5 - part 2

--- day5.py
+++ day5.py
@@ -1,6 +1,8 @@
 from utils import read_lines
 
 max_seat_id = 0
+min_seat_id = 127 * 8 + 7
+actual_seat_id_sum = 0
 
 for line in read_lines(5):
     min_row = 0
@@ -22,5 +24,14 @@ for line in read_lines(5):
     seat_id = min_row * 8 + min_col
     if seat_id > max_seat_id:
         max_seat_id = seat_id
+    if seat_id < min_seat_id:
+        min_seat_id = seat_id
 
+    actual_seat_id_sum += seat_id
 print(max_seat_id)
+
+sum_of_seat_ids_to_max = max_seat_id * (max_seat_id + 1) / 2 
+sum_of_seat_ids_below_min = (min_seat_id - 1) * (min_seat_id) / 2
+
+my_seat_id = sum_of_seat_ids_to_max - sum_of_seat_ids_below_min - actual_seat_id_sum
+print(my_seat_id)

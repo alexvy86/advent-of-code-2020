97123a3337fe8cfb2c2cb1be0b11a8c6a6f8fe01
Alex Villarreal <alex@alexvy86.com>
2020-12-05 22:17:55 +0000

    Day 5 - part 1

---
+++ day5.py
@@ -0,0 +1,26 @@
+from utils import read_lines
+
+max_seat_id = 0
+
+for line in read_lines(5):
+    min_row = 0
+    max_row = 127
+    min_col = 0
+    max_col = 7
+
+    for char in line:
+        if char == "F":
+            max_row = (max_row + min_row) // 2
+        elif char == "B":
+            min_row = (max_row + min_row) // 2 + 1
+        elif char == "L":
+            max_col = (max_col + min_col) // 2
+        elif char == "R":
+            min_col = (max_col + min_col) // 2 + 1
+        #print(f"{char} {min_row} {max_row} {min_col} {max_col}")
+
+    seat_id = min_row * 8 + min_col
+    if seat_id > max_seat_id:
+        max_seat_id = seat_id
+
+print(max_seat_id)

12369529ca8011c7098a8a9458cac8852b57112d
Alex Villarreal <alex@alexvy86.com>
2020-12-07 16:00:00 +0000

    Day 7 - part 2

--- day7.py
+++ day7.py
@@ -6,14 +6,20 @@ container_regex = re.compile(r"(?P<bag_type>\w* \w*) bags")
 contents_regex = re.compile(r"(?P<num>\d*) (?P<bag_type>\w* \w*) bags?")
 
 d = {}
+d2 = {}
 
 for line in read_lines(7):
 	pieces = line.split(" contain ")
 	container = container_regex.match(pieces[0])
 	contents = contents_regex.findall(pieces[1])
 
+	container_key = container.group("bag_type")
+	if container_key not in d2:
+		d2[container_key] = {}
+
 	for entry in contents:
 		key = entry[1]
+		d2[container_key][key] = entry[0]
 		if key not in d:
 			d[key] = []
 		d[key].append(container.group("bag_type"))
@@ -30,3 +36,16 @@ while not q.empty():
 		q.put(parent_bag)
 
 print(len(outer_bag_types))
+
+num_of_inner_bags = 0
+
+q = Queue()
+q.put("shiny gold")
+while not q.empty():
+	current = q.get()
+	for bag_type, num in d2.get(current, {}).items():
+		num_of_inner_bags += int(num)
+		for i in range(int(num)):
+			q.put(bag_type)
+
+print(num_of_inner_bags)

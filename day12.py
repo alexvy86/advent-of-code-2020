03edb13677f723335821bc031b3674eb8913e545
Alex Villarreal <alex@alexvy86.com>
2020-12-12 17:44:13 +0000

    Day 12 - part 2

--- day12.py
+++ day12.py
@@ -1,25 +1,38 @@
 from utils import read_lines
 
-current_pos = 0 + 0j
+current_pos = current_pos_2 = 0 + 0j
 current_direction = 1 + 0j
 rotations = {0: 1, 90: 1j, 180: -1, 270: -1j }
+
+waypoint_pos = 10 + 1j
 
 for instruction in read_lines(12):
 	op, num = (instruction[0], int(instruction[1:]))
 	#print(f"{current_pos} - {current_direction} - {op} {num}")
 	if op == "N":
 		current_pos += num * 1j
-	if op == "S":
+		waypoint_pos += num * 1j
+	elif op == "S":
 		current_pos -= num * 1j
-	if op == "E":
+		waypoint_pos -= num * 1j
+	elif op == "E":
 		current_pos += num
-	if op == "W":
+		waypoint_pos += num
+	elif op == "W":
 		current_pos -= num
-	if op == "F":
-		current_pos += current_direction * num
-	if op == "L":
-		current_direction *= rotations[num % 360]
-	if op == "R":
-		current_direction *= rotations[(360 - num) % 360]
+		waypoint_pos -= num
+	else:
+		waypoint_vector = (waypoint_pos - current_pos_2)
+		if op == "F":
+			current_pos += current_direction * num
+			current_pos_2 += num * waypoint_vector
+		elif op == "L":
+			current_direction *= rotations[num % 360]
+			waypoint_vector *= rotations[num % 360]
+		elif op == "R":
+			current_direction *= rotations[(360 - num) % 360]
+			waypoint_vector *= rotations[(360-num) % 360]
+		waypoint_pos = current_pos_2 + waypoint_vector
 
 print(abs(current_pos.real) + abs(current_pos.imag))
+print(abs(current_pos_2.real) + abs(current_pos_2.imag))

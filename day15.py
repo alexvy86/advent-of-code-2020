87dcd284a13f142ebfc9369ce17aa02dbc0b8f5f
Alex Villarreal <alex@alexvy86.com>
2020-12-15 23:20:29 +0000

    Day 15 - part 2

--- day15.py
+++ day15.py
@@ -9,10 +9,12 @@ for i, n in enumerate(starting_numbers):
 	spoken_numbers[n] = (i, None)
 	last_spoken_number = n
 
-for i in range(len(starting_numbers), 2020):
+for i in range(len(starting_numbers), 30000000):
 	new_number = 0 if spoken_numbers[last_spoken_number][1] == None \
 								 else spoken_numbers[last_spoken_number][0] - spoken_numbers[last_spoken_number][1]
 	spoken_numbers[new_number] = (i, spoken_numbers[new_number][0])
 	last_spoken_number = new_number
+	if i == 2019:
+		print(last_spoken_number)
 
 print(last_spoken_number)

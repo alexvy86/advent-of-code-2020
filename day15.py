6c2e574c98c8ac9057e21d11ecad4b6fc19688b3
Alex Villarreal <alex@alexvy86.com>
2020-12-15 22:51:18 +0000

    Day 15 - part 1

---
+++ day15.py
@@ -0,0 +1,18 @@
+from utils import read_lines
+from collections import defaultdict
+
+starting_numbers = [int(n) for n in read_lines(15)[0].split(",")]
+
+last_spoken_number = -1
+spoken_numbers = defaultdict(lambda: (None, None))
+for i, n in enumerate(starting_numbers):
+	spoken_numbers[n] = (i, None)
+	last_spoken_number = n
+
+for i in range(len(starting_numbers), 2020):
+	new_number = 0 if spoken_numbers[last_spoken_number][1] == None \
+								 else spoken_numbers[last_spoken_number][0] - spoken_numbers[last_spoken_number][1]
+	spoken_numbers[new_number] = (i, spoken_numbers[new_number][0])
+	last_spoken_number = new_number
+
+print(last_spoken_number)

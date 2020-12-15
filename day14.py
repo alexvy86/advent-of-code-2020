07e852688cce61e88fc07357523e7e4e3d413efe
Alex Villarreal <alex@alexvy86.com>
2020-12-15 22:14:33 +0000

    Day 14 - part 2

--- day14.py
+++ day14.py
@@ -14,3 +14,28 @@ for line in read_lines(14):
 		memory[index] = int(part2) & mask_1_and | mask_2_or
 
 print(sum(v for v in memory.values()))
+
+memory = {}
+x_count = 0
+for line in read_lines(14):
+	if line.startswith("mask"):
+		mask_str = line[7:]
+		x_count = mask_str.count("X")
+	else:
+		part1, part2 = line.split("] = ")
+		index = int(part1[4:])
+		index_str = list(format(index, f"036b"))
+
+		for i, c in enumerate(mask_str):
+			if c == "1" or c == "X":
+				index_str[i] = c
+
+		for i in range(pow(2,x_count)):
+			t = "".join(index_str)
+			for c in format(i, f"0{x_count}b"):
+				t = t.replace("X", c, 1)
+			
+			masked_index = int(t, 2)
+			memory[masked_index] = int(part2)
+
+print(sum(v for v in memory.values()))

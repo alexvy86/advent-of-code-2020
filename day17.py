548c4d6f078b0def25e7daa2853003d29dc87d2e
Alex Villarreal <alex@alexvy86.com>
2020-12-17 23:16:45 +0000

    Day 17 - part 2

--- day17.py
+++ day17.py
@@ -1,50 +1,43 @@
 from collections import defaultdict
+import itertools
 
-initial_state = [
-	"####.#..",
-	".......#",
-	"#..#####",
-	".....##.",
-	"##...###",
-	"#..#.#.#",
-	".##...#.",
-	"#...##.."]
+initial_state = [	"####.#..",
+									".......#",
+									"#..#####",
+									".....##.",
+									"##...###",
+									"#..#.#.#",
+									".##...#.",
+									"#...##.."]
 
-neighbors = [(z,x,y) for x in range(-1,2) for y in range(-1,2) for z in range(-1,2) if (z,x,y) != (0,0,0)]
+dimensions = 4
+d_min_max = [[0,0] for _ in range(dimensions)]
+neighbor_offsets = list(list(x) for x in itertools.product([-1,0,1], repeat=dimensions) if x != tuple([0]*dimensions))
 
 def next_state(cell, cube):
-	alive_neighbors = sum(1 for (z,x,y) in neighbors if cube[cell[0] + z][cell[1] + x][cell[2] + y] == "#")
-	if cube[cell[0]][cell[1]][cell[2]] == "#":
-		return "#" if alive_neighbors in [2,3] else "."
-	else:
-		return "#" if alive_neighbors in [3] else "."
+	alive_neighbors = 0
+	for offset in neighbor_offsets:
+		neighbor = cube[tuple(cell[i] + offset[i] for i in range(dimensions))]
+		if neighbor == "#":
+			alive_neighbors += 1
 
-infinite_cube = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: '.')))
-for x, row in enumerate(initial_state):
-	for y, col in enumerate(row):
-		infinite_cube[0][x][y] = col
+	return "#" if (cube[cell] == "#" and alive_neighbors in [2,3]) or (cube[cell] == "." and alive_neighbors in [3]) else "."
 
-z_min = z_max = x_min = y_min = 0
-x_max = len(initial_state) - 1
-y_max = len(initial_state[0]) - 1
+infinite_cube = defaultdict(lambda: '.', {tuple([0]*(dimensions-2)) + (x,y): col for x, row in enumerate(initial_state) for y, col in enumerate(row)})
+
+d_min_max[-2][1] = len(initial_state) - 1
+d_min_max[-1][1] = len(initial_state[0]) - 1
 
 for i in range(6):
-	#print(f"Step {i}")
-	new_cube = defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: '.')))
-	for z in range(z_min - 1, z_max + 2):
-		for y in range(y_min - 1, y_max + 2):
-			for x in range(x_min - 1, x_max + 2):
-				new_cube[z][x][y] = next_state((z, x, y), infinite_cube)
-				if new_cube[z][x][y] == "#":
-					z_min = min(z_min, z)
-					z_max = max(z_max, z)
-					x_min = min(x_min, x)
-					x_max = max(x_max, x)
-					y_min = min(y_min, y)
-					y_max = max(y_max, y)
-	infinite_cube = new_cube
-	# for z in range(min_z, max_z + 1):
-	# 	for x in range(min_x, max_x + 1):
-	# 		print("".join(cell for cell in infinite_cube[z][x].values()))
+	new_cube = defaultdict(lambda: '.')
+	for coords in itertools.product(*(list(range(my_min - 1, my_max + 2)) for my_min, my_max in d_min_max)):
+		new_cube[coords] = next_state(coords, infinite_cube)
+		if new_cube[coords] == "#":
+			for d, c in enumerate(coords):
+				d_min_max[d][0] = min(d_min_max[d][0], c)
+				d_min_max[d][1] = max(d_min_max[d][1], c)
 
-print(sum(1 for x in range(x_min,x_max+1) for y in range(y_min,y_max+1) for z in range(z_min,z_max+1) if infinite_cube[z][x][y] == "#"))
+	infinite_cube = new_cube
+
+all_cube_coords = [c for c in itertools.product(*(list(range(my_min, my_max + 1)) for my_min, my_max in d_min_max))]
+print(sum(1 for c in all_cube_coords if infinite_cube[c] == "#"))

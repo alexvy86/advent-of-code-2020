9c444767caa874fa5d511dce976cc5dc200c2112
Alex Villarreal <716334+alexvy86@users.noreply.github.com>
2020-12-20 04:00:48 +0000

    Day 18 - part 2

--- day18.py
+++ day18.py
@@ -26,4 +26,36 @@ def evaluate(expr):
 
 	return (running_result, len(expr))
 
+def evaluate2(expr):
+	i = 0
+	factors = []
+	operator = "+"
+	running_result = 0
+	while i < len(expr):
+		if expr[i] in ["+", "*"]:
+			operator = expr[i]
+			i += 1
+			continue
+		
+		if expr[i] == ")":
+			return (prod(factors + [running_result]), i + 1)
+		
+		if expr[i] == "(":
+			next_number, consumed_chars = evaluate2(expr[i+1:])
+			i += consumed_chars			
+		else:
+			next_number = int(expr[i])
+
+		if operator == "+":
+			running_result += next_number
+		elif operator == "*":
+			factors.append(running_result)
+			running_result = next_number
+			operator = "+" 
+		
+		i += 1
+
+	return (prod(factors + [running_result]), len(expr))
+
 print(sum(evaluate(line.replace(" ", ""))[0] for line in read_lines(18)))
+print(sum(evaluate2(line.replace(" ", ""))[0] for line in read_lines(18)))
